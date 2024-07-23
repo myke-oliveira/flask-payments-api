9a5616fbff0f2a72cfb7598d17f78108f508a749
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2024-07-23 22:39:59 +0000

    🗄️ DB: initial config

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, jsonify
+from repository.database import db
+from models.payment import Payment
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+app.config['SECRET_KEY'] = "your_secret_key"
+
+db.init_app(app)
 
 @app.route("/payments/pix", methods=["POST"])
 def create_payment_pix():
