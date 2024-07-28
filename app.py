dd98352659d167b5d5d57d5cb76bd03be01b44b7
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2024-07-28 18:51:32 +0000

    🆕 NEW: handle websocket disconnection

--- app.py
+++ app.py
@@ -89,6 +89,10 @@ def payment_pix_page(payment_id):
 @socketio.on("connect")
 def handle_connect():
     print("Client connected to the server!!!")
+    
+@socketio.on("disconnect")
+def handle_disconnect():
+    print("Client disconnected!!!]")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
